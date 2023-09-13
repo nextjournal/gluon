15957952834e3edf0325e1e65d139c241961b751
Martin Kavalar <martin@nextjournal.com>
2023-09-13 16:17:33 +0200

    Add garden-id example

---
+++ examples/garden-id/src/example/garden_id.clj
@@ -0,0 +1,25 @@
+;; ## 🔑 Garden ID
+
+(ns example.garden-id
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.viewer :as viewer]
+            [nextjournal.gluon :as gluon]))
+
+
+(defn build-session-state [opts]
+  opts)
+
+(defn render-page [system {:as session-state :keys [current-user]}]
+  (clerk/html [:div.mt-6 [:h1 "🔑 Garden ID"]
+               (if current-user
+                 [:div
+                  [:div {:nextjournal/value session-state}]
+                  #_[:a {:data-ignore-anchor-click true :href "/logout"} "Logout"]] ;; TODO
+                 [:a {:data-ignore-anchor-click true :href "/login"} "Login"])]))
+
+(comment
+  (some-> 'gluon resolve deref :instance org.httpkit.server/server-stop!)
+  (def gluon
+    (gluon/serve! {:port 8889 :garden-id true :ns *ns*}))
+
+  )
