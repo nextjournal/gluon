52cae3317cb143aaefd65888dc99d55b08472e22
Philippa Markovics <philippa@markovics.com>
2023-09-18 17:36:16 -0700

    Examples index overhaul

--- examples/index.clj
+++ examples/index.clj
@@ -7,9 +7,12 @@
 (defn example [[name {:as opts :keys [config]}]]
   (let [url (str "http://localhost:" (:port config))]
     (clerk/fragment
-     (clerk/html [:div.p-1.font-mono.text-xs [:a {:href url} url]])
      (clerk/html {::clerk/width :full}
-                 [:iframe.w-full.border {:src url}])))
+                 [:div.max-w-prose.mx-auto
+                  [:iframe.w-full.rounded-md.border.shadow-lg
+                   {:src url
+                    :style {:min-height 170}}]
+                  [:div.px-6.py-2.font-mono.text-xs.text-right [:a {:href url} url]]])))
   )
 
 {:nextjournal.clerk/visibility {:result :show}}
